05c177e5697cdee7e06a3e41a3387b435d5870f7
Armando Aguiar Alamilla <31995672+armandoalamilla@users.noreply.github.com>
2019-10-31 15:30:20 -0600

    cuadruplos funcionado
    + o -
    * o /
    asignacion
    escritura
    lectura

--- cuadruplos.py
+++ cuadruplos.py
@@ -15,13 +15,14 @@ PQuad = []
 #contador cuadruplos
 contCuad = 0
 
+right_operand = ''
+right_type = ''
+left_operand = ''
+left_type = ''
+
 def agregarCuad(operator, left_operand, right_operand, result):
     PQuad.append({'operator':operator, 'left_operand':left_operand,'right_operand':right_operand, 'result':result})
     global contCuad
     contCuad = contCuad + 1
 
 
-agregarCuad('-','a','2','t1')
-agregarCuad('=','t1','','i')
-
-print(PQuad[1])
--- lexer_parser.py
+++ lexer_parser.py
@@ -1,6 +1,7 @@
 import ply.lex as lex
 import ply.yacc as yacc
-import dirFunciones as directorio
+import dirFunciones as directorio, cuadruplos as cuad
+import cuboSemantico as cubo
 import sys, json, os
 
 aprobado = True
@@ -13,8 +14,14 @@ global varTipoActual, tipoTemp
 global NombreFuncActual, scopeActual, tipoActual, tempNombreFunc
 
 nombreFunc = ''
+tempCTE = 'TEMPCTEAAAAAAAAA'
+tempSigno = ''
 
 arrayNombreFunc = []
+
+temTipoCTE = ''
+
+
 
 #contadores
 contadorScope = 0
@@ -38,6 +45,7 @@ idTemp_modulos = ''
 NombreFuncActual = ''
 scopeActual = ''
 tipoActual = ''
+tempNombreVar = ''
 
 #directorio.almacenaFuncion(NombreFuncActual,scopeActual,tipoActual)
 
@@ -115,27 +123,48 @@ t_COMA = r'\,'
 t_PUNTOYCOMA = r'\;'
 t_DOSPUNTOS = r'\:'
 t_IGUAL = r'\='
-t_CTE_I = r'[0-9]+'
-t_CTE_F = r'[0-9]+\.[0-9]+'
-
-
-t_CTE_CHAR = r'\'' + r'([a-zA-Z_][a-zA-Z0-9_]*|.*|/*)' + r'\''
-
+#t_CTE_I = r'[0-9]+'
+#t_CTE_F = r'[0-9]+\.[0-9]+'
+#t_CTE_CHAR = r'(\'[^\']*\')'
 
 
 tokens = tokens + list(reserved.values())
+
+def t_CTE_CHAR(t):
+    r'(\'[^\']*\')'
+    global temTipoCTE
+    temTipoCTE = 'CHAR'
+    return t
+
+
+def t_CTE_F(t):
+    r'[0-9]+\.[0-9]+'
+    t.value = float(t.value)
+    global temTipoCTE
+    temTipoCTE = 'FLOAT'
+    return t
+
+def t_CTE_I(t):
+    r'[0-9]+'
+    t.value = int(t.value)
+    global temTipoCTE
+    temTipoCTE = 'INT'
+    return t
+
 
 def t_ID(t):
     r'[a-zA-Z][a-zA-Z0-9]*'
     t.type = reserved.get(t.value,'ID')
     return t
 
+
+
 #caracteres que se ignoran
 
 t_ignore = ' \t\n'
 
-def t_error(t):   
-    global aprobado 
+def t_error(t):
+    global aprobado
     aprobado = False
     print("Caracter malo '%s'" % t.value[0])
     t.lexer.skip(1)
@@ -145,10 +174,10 @@ def t_error(t):
 lex.lex()
 
 def p_programa(p):
-    '''programa : REGLA_PROGRAMA ID DOSPUNTOS pN1 REGLA_MAIN pN11 bloque REGLA_END 
+    '''programa : REGLA_PROGRAMA ID DOSPUNTOS pN1 REGLA_MAIN pN11 bloque REGLA_END
                 | REGLA_PROGRAMA ID DOSPUNTOS pN1 vars REGLA_MAIN pN11 bloque REGLA_END
                 | REGLA_PROGRAMA ID DOSPUNTOS pN1 programa_modulos_aux REGLA_MAIN pN11 bloque REGLA_END
-                | REGLA_PROGRAMA ID DOSPUNTOS pN1 vars  programa_modulos_aux REGLA_MAIN pN11 bloque REGLA_END  
+                | REGLA_PROGRAMA ID DOSPUNTOS pN1 vars  programa_modulos_aux REGLA_MAIN pN11 bloque REGLA_END
     '''
 
 # pN1: almacena el main en el dir de funciones
@@ -165,8 +194,8 @@ def p_pN11(p):
     nombreFunc = 'MAIN'
 
 
-    
-  
+
+
 def p_programa_modulos_aux(p):
     ''' programa_modulos_aux : modulos
                             | modulos programa_modulos_aux'''
@@ -180,25 +209,25 @@ def p_modulos(p):
 def p_modulos_aux(p):
     ''' modulos_aux : pN6 tipo pN5
                     | pN6 tipo COMA pN5 modulos_aux '''
-    
-    #print(p[1],tempTipoVarFuncEntrada)            
+
+    #print(p[1],tempTipoVarFuncEntrada)
 
 #agrega el modulo -- punto neuralgico 3
 def p_pN3(p):
     '''pN3 : '''
     global contadorScope, idTemp_modulos, tempTipo_modulos, arrayNombreFunc, nombreFunc
     #print(contadorScope, p[4])
-    #arrayNombreFunc.append(idTemp_modulos)       
+    #arrayNombreFunc.append(idTemp_modulos)
     scopeActual = 'LOCAL'
     #print(nombreFunc, tempTipo_modulos)
     directorio.almacenaFuncion(nombreFunc,scopeActual,tempTipo_modulos)
-   
+
 
 #almacenar ID de modulo -- punto neuralgico 4
 def p_pN4(p):
     ''' pN4 : ID '''
     global nombreFunc
-    nombreFunc = p[1] 
+    nombreFunc = p[1]
 
 
 #almacenar variables de entrada de funciones -- punto neuralgico 5
@@ -254,20 +283,20 @@ def p_pN10(p):
 
 
 
-        
- 
-   
 
 
-    
-    
-    
+
+
+
+
+
+
 
 def p_vars(p):
     ''' vars : REGLA_VAR declaracionVar
                 | REGLA_VAR declaracionVar vars'''
 
-        
+
 
 
 def p_tipo(p):
@@ -280,15 +309,15 @@ def p_tipo(p):
     global contadorINT, contadorFLOAT, contadorCHAR, contadorDATASET, contadorBOOL, interruptorVARID, contadorVARID
 
     tempTipoVarFuncEntrada = p[1]
-    
+
     #print(contadorScope, arrayNombreFunc)
     #print(arrayNombreFunc,contadorScope)
     for x in varNombreTemp:
-        #print(x)      
-        directorio.almacenaVarsEnFunc(nombreFunc,x,p[1])        
+        #print(x)
+        directorio.almacenaVarsEnFunc(nombreFunc,x,p[1])
     varNombreTemp.clear()
 
-    #contar el numero de tipos para paremetros - func 
+    #contar el numero de tipos para paremetros - func
     if directorio.funcionLista[nombreFunc]['scope'] == 'LOCAL' and interruptorVARID == False:
         if p[1] == 'INT':
             contadorINT = contadorINT + 1
@@ -313,25 +342,25 @@ def p_tipo(p):
             contadorDATASET = contadorVARID
         interruptorVARID = False
         contadorVARID = 0
-        
-        
-    
-
-    
-    
 
 
 
 
 
-       
-    
-    
-      
-    
-   
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def p_tipo_func(p):
     '''tipo_func : REGLA_INT
@@ -339,12 +368,12 @@ def p_tipo_func(p):
         | REGLA_CHAR
         | REGLA_BOOL
         | REGLA_VOID '''
-    global tempTipo_modulos   
+    global tempTipo_modulos
     tempTipo_modulos = p[1]
     #print("tipo modulo",p[1])
     #directorio.almacenaFuncion(NombreFuncActual,scopeActual,tipoActual)
-    
-    
+
+
 
 def p_expresion(p):
     '''expresion : exp MAYORQUE exp
@@ -354,12 +383,14 @@ def p_expresion(p):
                    | exp IGUALIGUAL exp
                    | exp DIFDE exp
                    | exp '''
+    
 
 def p_logical_expresion(p):
     '''logical_expresion : REGLA_NOT expresion
         | expresion REGLA_AND expresion
-        | expresion REGLA_OR expresion 
+        | expresion REGLA_OR expresion
         | expresion '''
+    
 
 def p_estatuto(p):
     '''estatuto : llamada_funcion
@@ -379,10 +410,18 @@ def p_llamada_funcion_aux(p):
 
 def p_escritura(p):
     ''' escritura : REGLA_PRINT ABREPAR escritura_aux CIERRAPAR PUNTOYCOMA '''
+    
 
 def p_escritura_aux(p):
-    ''' escritura_aux : expresion 
-                        | expresion COMA escritura_aux '''
+    ''' escritura_aux : expresion pN19
+                        | expresion COMA pN19 escritura_aux '''
+
+#genera cuad de escritura -- pN19
+def p_pN19(p):
+    ''' pN19 : '''
+    cuad.PTypes.pop()
+    cuad.agregarCuad('PRINT','',cuad.PilaO.pop(),'t'+str(cuad.contCuad))
+
 
 def p_condicion(p):
     '''condicion : REGLA_IF ABREPAR logical_expresion CIERRAPAR bloque PUNTOYCOMA
@@ -390,22 +429,57 @@ def p_condicion(p):
         '''
 
 def p_asignacion(p):
-    '''asignacion : pN12 IGUAL logical_expresion PUNTOYCOMA
-        | pN12 array IGUAL logical_expresion PUNTOYCOMA
-        '''
-
-#verifica que la variable exista en el dir de funciones -- punto neuralgico 12
-def p_pN12(p):
-    ''' pN12 : ID '''
+    '''asignacion : pN12 pN16 logical_expresion PUNTOYCOMA
+        | pN12 array pN16 logical_expresion PUNTOYCOMA '''
     global nombreFunc
 
+    
+    if cuad.POper[len(cuad.POper)-1] == '=':
+        cuad.right_operand = cuad.PilaO.pop()
+        cuad.right_type = cuad.PTypes.pop()
+        cuad.left_operand = cuad.PilaO.pop()
+        cuad.left_type = cuad.PTypes.pop()
+        operator = cuad.POper.pop()        
+        result_type = cubo.sem_cubo[cuad.left_type][cuad.right_type][operator]
+        if result_type != 'error':
+            result = 't'+str(cuad.contCuad)
+            cuad.agregarCuad(operator,cuad.left_operand,cuad.right_operand,result)
+            cuad.PilaO.append(result)
+            cuad.PTypes.append(result_type)
+            #print('asignacion',cuad.PTypes)
+            #print('asignacion',cuad.PilaO)
+            #print('asignacion',cuad.POper)
+            #cuad.PilaO.clear()
+            #cuad.PTypes.clear()
+        else:
+            print("HORROR DE TIPOS")
+            sys.exit()
+
+
+#almacenar igual en POPer -- punto neuralgico 16
+def p_pN16(p):
+    ''' pN16 : IGUAL '''
+    cuad.POper.append(p[1])
+
+
+
+#verifica que la variable exista en el dir de funciones y almacenar en la pila de operandos -- punto neuralgico 12
+def p_pN12(p):
+    ''' pN12 : ID '''
+    global nombreFunc, tempCTE, tempNombreVar
+    
     try:
         directorio.funcionLista[nombreFunc]['variables'][p[1]]
     except KeyError:
         print('variable',p[1],'en',nombreFunc,'no esta declarada')
         sys.exit()
-
+    else:
+        cuad.PilaO.append(p[1])
+        tempNombreVar = p[1]
+        cuad.PTypes.append(directorio.funcionLista[nombreFunc]['variables'][p[1]]['tipo'])
     
+
+
 
 def p_ciclo(p):
     '''ciclo : REGLA_WHILE ABREPAR logical_expresion CIERRAPAR bloque PUNTOYCOMA
@@ -427,6 +501,7 @@ def p_lectura(p):
     ''' lectura : REGLA_READ ABREPAR ID CIERRAPAR PUNTOYCOMA
         | REGLA_READ ABREPAR ID array CIERRAPAR PUNTOYCOMA
         '''
+    cuad.agregarCuad('READ','',p[3],'t'+str(cuad.contCuad))
 def p_array(p):
     ''' array : ABREBRACK exp CIERRABRACK
         | ABREBRACK exp CIERRABRACK ABREBRACK exp CIERRABRACK
@@ -441,8 +516,8 @@ def p_declaracionVar(p):
                         | ID ABREBRACK CTE_I CIERRABRACK ABREBRACK CTE_I CIERRABRACK DOSPUNTOS  REGLA_FLOAT PUNTOYCOMA
                         | ID ABREBRACK CTE_I CIERRABRACK ABREBRACK CTE_I CIERRABRACK DOSPUNTOS REGLA_CHAR PUNTOYCOMA
                         '''
-    global nombreFunc, contadorINT, contadorFLOAT, contadorBOOL, contadorCHAR, contadorDATASET 
-        
+    global nombreFunc, contadorINT, contadorFLOAT, contadorBOOL, contadorCHAR, contadorDATASET
+
 
     if p[1] != None :
         if p[5] == '[' : #verifica si es matrix
@@ -463,29 +538,52 @@ def p_declaracionVar(p):
                 contadorCHAR = contadorCHAR + 1
 
 
-    
 
-        
-def p_var_id(p):    
+
+
+def p_var_id(p):
     ''' var_id : ID
                 | ID COMA var_id
                 '''
     global varNombreTemp, interruptorVARID, contadorVARID
     contadorVARID = contadorVARID + 1
     interruptorVARID = True
-    varNombreTemp.append(p[1]) 
-    
+    varNombreTemp.append(p[1])
+
 
 def p_var_cte(p):
-    ''' var_cte : pN12 
-                | pN12 array
-                | pN13 ABREPAR var_cte_aux CIERRAPAR 
+    ''' var_cte : pN20
+                | pN20 array
+                | pN13 ABREPAR var_cte_aux CIERRAPAR
                 | CTE_I
                 | CTE_F
                 | CTE_CHAR
                 | CTE_BOOL
                 | CTE_D
                 '''
+    global tempCTE, tempType
+
+    if p[1] != None:
+        cuad.PilaO.append(p[1])
+        #tempType = p[1]
+
+def p_pN20(p):
+    ''' pN20 : ID '''
+    global nombreFunc, tempCTE, tempNombreVar, temTipoCTE
+    
+    try:
+        directorio.funcionLista[nombreFunc]['variables'][p[1]]
+    except KeyError:
+        print('variable',p[1],'en',nombreFunc,'no esta declarada')
+        sys.exit()
+    else:
+        cuad.PilaO.append(p[1])
+        tempNombreVar = p[1]
+        temTipoCTE = directorio.funcionLista[nombreFunc]['variables'][p[1]]['tipo']
+    
+
+
+
 
 #checa que la funcion este declarada en el dir de funciones -- punto neuralgico 13
 def p_pN13(p):
@@ -497,7 +595,9 @@ def p_pN13(p):
         print('La funcion',p[1],'en',nombreFunc,'no esta declarada')
         sys.exit()
 
-    
+
+
+
 
 def p_var_cte_aux(p):
     ''' var_cte_aux : exp
@@ -505,31 +605,104 @@ def p_var_cte_aux(p):
 
 def p_bloque(p):
     ''' bloque : ABRECOR estatuto_aux CIERRACOR
-                
+
     '''
 
 def p_estatuo_aux(p):
-    ''' estatuto_aux : estatuto 
+    ''' estatuto_aux : estatuto
                     | estatuto estatuto_aux '''
 
 def p_exp(p):
-    ''' exp : termino 
-            | termino SUMA exp 
-            | termino RESTA exp '''
-    
+    ''' exp : termino pN17
+            | termino pN17 pN14 exp '''
+
+#almacenar el simbolo de suma y resta en la pila POper -- punto neuralgico 14
+def p_pN14(p):
+    ''' pN14 : SUMA
+             | RESTA '''
+    cuad.POper.append(p[1])
+
+#checa la semantica cuando es + o - -- pN 17
+def p_pN17(p):
+    ''' pN17 : '''
+    try:
+        cuad.POper[len(cuad.POper)-1]
+    except IndexError:
+        print("pila vacia")
+    else:
+        if cuad.POper[len(cuad.POper)-1] == '+' or cuad.POper[len(cuad.POper)-1] == '-':
+            cuad.right_operand = cuad.PilaO.pop()
+            cuad.right_type = cuad.PTypes.pop()
+            cuad.left_operand = cuad.PilaO.pop()
+            cuad.left_type = cuad.PTypes.pop()
+            operator = cuad.POper.pop()
+            result_type = cubo.sem_cubo[cuad.left_type][cuad.right_type][operator]
+            if result_type != 'error':
+                result = 't'+str(cuad.contCuad)
+                cuad.agregarCuad(operator,cuad.left_operand,cuad.right_operand,result)
+                cuad.PilaO.append(result)
+                cuad.PTypes.append(result_type)
+            else:
+                print("HORROR DE TIPOS")
+                sys.exit()
+
+
+
+
+
+
 
 def p_termino(p):
-    ''' termino : factor 
-                | factor MULTIPLICACION termino
-                | factor DIVISION termino '''
+    ''' termino : factor pN18
+                | factor pN18 pN15 termino '''
+
+#almacenar * o / en POper -- punto neuralgico 15
+def p_pN15(p):
+    ''' pN15 : MULTIPLICACION
+            | DIVISION '''
+
+    cuad.POper.append(p[1])
+
+#checa la semantica cuando es + o - -- pN 18
+def p_pN18(p):
+    ''' pN18 : '''
+    try:
+        cuad.POper[len(cuad.POper)-1]
+    except IndexError:
+        print("pila vacia")
+    else:
+        if cuad.POper[len(cuad.POper)-1] == '*' or cuad.POper[len(cuad.POper)-1] == '/':
+            cuad.right_operand = cuad.PilaO.pop()
+            cuad.right_type = cuad.PTypes.pop()
+            cuad.left_operand = cuad.PilaO.pop()
+            cuad.left_type = cuad.PTypes.pop()
+            operator = cuad.POper.pop()
+            result_type = cubo.sem_cubo[cuad.left_type][cuad.right_type][operator]
+            if result_type != 'error':
+                result = 't'+str(cuad.contCuad)
+                cuad.agregarCuad(operator,cuad.left_operand,cuad.right_operand,result)
+                cuad.PilaO.append(result)
+                cuad.PTypes.append(result_type)
+            else:
+                print("HORROR DE TIPOS")
+                sys.exit()
+
+
 
 def p_factor(p):
-    ''' factor : ABREPAR logical_expresion CIERRAPAR 
-                | var_cte 
-                | SUMA var_cte 
-                | RESTA var_cte '''
+    ''' factor : ABREPAR logical_expresion CIERRAPAR
+                | var_cte pN2
+                | SUMA var_cte
+                | RESTA var_cte'''
+
+#meter el tipo en la pila de operandos -- punto neuralgico 2
+def p_pN2(p):
+    ''' pN2 : '''
+    global tempCTE, temTipoCTE
+    cuad.PTypes.append(temTipoCTE)
 
 
+  
 
 
 def p_error(p):
@@ -547,6 +720,10 @@ s = f.read()
 parser.parse(s)
 
 
+
+
+
+
 #imprimir dir de funciones
 app_json = json.dumps(directorio.funcionLista, indent=4)
 #pp_json2 = json.dumps(directorio.lista_vars, indent=4)
@@ -559,6 +736,13 @@ f.close()
 #print(arrayNombreFunc)
 
 
+print(cuad.POper)
+print(cuad.PTypes)
+print(cuad.PilaO)
+for x in cuad.PQuad:
+    print(x)
+
+
 
 if aprobado == True:
     print("Archivo APROBADO")
@@ -566,7 +750,3 @@ if aprobado == True:
 else:
     print("Archivo :(")
     sys.exit()
-
-
-
-
