bca2ed17fc31ba11d3e1c0edf8b7e679bb147103
Lucia <luciaoseguera@MAcs-MacBook-Pro-Lucia-2.local>
2019-11-19 18:27:31 -0600

    Cambios en READ MV

--- maquinaVirtual.py
+++ maquinaVirtual.py
@@ -1,10 +1,16 @@
 import cuadruplos as cuad
 import memoria as mem
 import dirFunciones as directorio
+from ast import literal_eval
 cuadActual = 0
+
+global direccionRead , valorRead, valorTipo, inputVal
+
+
 
 def maquinaVirtual():
     global cuadActual
+
     while cuadActual < cuad.contQuadAux:
         if cuad.PQuad[cuadActual]['operator'] == 'GOTO':
             cuadActual = cuad.PQuad[cuadActual]['result']
@@ -12,12 +18,41 @@ def maquinaVirtual():
             print("IMPRIME CONSOLA:",mem.obtenerValordeMemoria(cuad.PQuad[cuadActual]['result']))
             cuadActual += 1
         elif cuad.PQuad[cuadActual]['operator'] == 'READ':
-            mem.almacenaMemoriaEjecucion(cuad.PQuad[cuadActual]['result'],input("INGRESA EL VALOR DE LA VAR: "))
+            direccionRead = cuad.PQuad[cuadActual]['result'];
+            valorRead = input("INGRESA EL VALOR DE LA VAR: ");
+
+            def get_type(input_data): #Identifica que tipo es lo que se ingreso'
+                try:
+                    return type(literal_eval(input_data))
+                except (ValueError, SyntaxError):
+                    # A string, so return str
+                    return str
+
+            valorTipo = str(get_type(valorRead)) #Aqui guarda string
+                                                 #<class 'int'>
+                                                 #<class 'float'>
+                                                 #<class 'bool'>
+                                                 #<class 'string'>
+                                                 #dependiendo lo ingresado
+            if (valorTipo[8] == 'i'):
+                print('es int')
+                inputVal = int(valorRead)
+            elif (valorTipo[8] == 'f'):
+                print('es float')
+                inputVal = float(valorRead)
+            elif (valorTipo[8] == 'b'):
+                print('es bool')
+                inputVal = bool(valorRead)
+            else:
+                print('es string')
+                inputVal = valorRead
+
+            mem.almacenaMemoriaEjecucion(direccionRead ,inputVal)
             cuadActual += 1
         elif cuad.PQuad[cuadActual]['operator'] == '=':
             direccionVar = cuad.PQuad[cuadActual]['result']
             valor = mem.obtenerValordeMemoria(cuad.PQuad[cuadActual]['left_operand'])
-            mem.almacenaMemoriaEjecucion(direccionVar,valor)                     
+            mem.almacenaMemoriaEjecucion(direccionVar,valor)
             cuadActual += 1
         elif cuad.PQuad[cuadActual]['operator'] == '+':
             resultado = mem.obtenerValordeMemoria(cuad.PQuad[cuadActual]['left_operand']) + mem.obtenerValordeMemoria(cuad.PQuad[cuadActual]['right_operand'])
@@ -77,8 +112,3 @@ def maquinaVirtual():
                 cuadActual += 1
         else:
             cuadActual+=1
-        
-        
-
-
-            
