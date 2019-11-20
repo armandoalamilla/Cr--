d72b850a2cc43d4ad2ece02dcf0ceaf0799008f4
Lucia <luciaoseguera@MAcs-MacBook-Pro-Lucia-2.local>
2019-11-19 20:11:33 -0600

    READ con validacion

--- maquinaVirtual.py
+++ maquinaVirtual.py
@@ -2,6 +2,8 @@ import cuadruplos as cuad
 import memoria as mem
 import dirFunciones as directorio
 from ast import literal_eval
+import sys
+
 cuadActual = 0
 
 global direccionRead , valorRead, valorTipo, inputVal
@@ -19,6 +21,7 @@ def maquinaVirtual():
             cuadActual += 1
         elif cuad.PQuad[cuadActual]['operator'] == 'READ':
             direccionRead = cuad.PQuad[cuadActual]['result'];
+            print(direccionRead)
             valorRead = input("INGRESA EL VALOR DE LA VAR: ");
 
             def get_type(input_data): #Identifica que tipo es lo que se ingreso'
@@ -34,18 +37,42 @@ def maquinaVirtual():
                                                  #<class 'bool'>
                                                  #<class 'string'>
                                                  #dependiendo lo ingresado
+
+            #En esta parte se comprueba que el tipo del valor ingresado concuerde
+            #con el tipo de la variable ya antes declarada, si es asi, el valor
+            #es convertido a el tipo correspondiente y lo almacena en memoria
             if (valorTipo[8] == 'i'):
-                print('es int')
-                inputVal = int(valorRead)
+                print('Lo que se ingresó es tipo int')
+                if (direccionRead >= 1000 and direccionRead < 3000) | (direccionRead >= 11000 and direccionRead < 13000):
+                    print("Si concuerda con el tipo de la variable")
+                    inputVal = int(valorRead)
+                else:
+                    print("HORROR DE TIPOS")
+                    sys.exit()
             elif (valorTipo[8] == 'f'):
-                print('es float')
-                inputVal = float(valorRead)
+                print('Lo que se ingresó es tipo float')
+                if (direccionRead >= 3000 and direccionRead < 5000) | (direccionRead >= 13000 and direccionRead < 15000):
+                    print("Si concuerda con el tipo de la variable")
+                    inputVal = float(valorRead)
+                else:
+                    print("HORROR DE TIPOS")
+                    sys.exit()
             elif (valorTipo[8] == 'b'):
-                print('es bool')
-                inputVal = bool(valorRead)
+                print('Lo que se ingresó es tipo bool')
+                if (direccionRead >= 5000 and direccionRead < 7000) | (direccionRead >= 15000 and direccionRead < 17000):
+                    print("Si concuerda con el tipo de la variable")
+                    inputVal = bool(valorRead)
+                else:
+                    print("HORROR DE TIPOS")
+                    sys.exit()
             else:
-                print('es string')
-                inputVal = valorRead
+                print('Lo que se ingresó es tipo string')
+                if (direccionRead >= 7000 and direccionRead < 9000) | (direccionRead >= 17000 and direccionRead < 19000):
+                    print("Si concuerda con el tipo de la variable")
+                    inputVal = valorRead
+                else:
+                    print("HORROR DE TIPOS")
+                    sys.exit()
 
             mem.almacenaMemoriaEjecucion(direccionRead ,inputVal)
             cuadActual += 1
